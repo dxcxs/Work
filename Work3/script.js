21fcb0293a46b9c1550cdb60263ba59d1ae3a9be
dxcxs <mxgot6@gmail.com>
2026-06-29 12:06:42 +0700

    Add files via upload

---
+++ Work3/script.js
@@ -0,0 +1,9 @@
+for (i=1; i<=5; i++) {
+    document.write(`<h${i}>สวัสดี x${i} <br>`);
+}
+
+for (i=1; i<10; i++) {
+    document.write("<br>")
+    document.write("<img src='image/cartoon.gif' width='60'>".repeat(i));
+}
+
